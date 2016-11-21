f603472350d63ae41fce0ed7fdce55e8c69df1ed
lili <liq1862@gmail.com>
2016-11-21 11:23:30 +0800

    CreatOptionsMenu show

--- app/src/main/java/com/example/user/a20161121_test1/MainActivity.java
+++ app/src/main/java/com/example/user/a20161121_test1/MainActivity.java
@@ -4,15 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView lv;
+    MyAdapter adapter;
     String fruits[] = {"蘋果", "西瓜", "楊桃", "鳳梨", "蘋果1", "西瓜1", "楊桃1", "鳳梨1", "蘋果2", "西瓜2", "楊桃2", "鳳梨2", "蘋果3", "西瓜3", "楊桃3", "鳳梨3"};
 
     @Override
@@ -21,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lv = (ListView) findViewById(R.id.listView);
-        MyAdapter adapter = new MyAdapter(MainActivity.this,fruits);
+        adapter = new MyAdapter(MainActivity.this,fruits);
         lv.setAdapter(adapter);
 
     }
@@ -30,5 +33,21 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         menu.add("show");
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i=0;i<fruits.length;i++)
+        {
+            if (adapter.chks[i])
+            {
+                sb.append(fruits[i] + " ");
+            }
+        }
+
+        Toast.makeText(MainActivity.this, sb.toString(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
